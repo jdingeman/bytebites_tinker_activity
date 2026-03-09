d1c734578a7f444866f6b60a7bfe7cb5407f024d
justin <jmdingeman@gmail.com>
2026-03-08 20:27:38 -0700

    add empty models.py with class list

---
+++ models.py
@@ -0,0 +1,8 @@
+'''
+Implemented classes for the ByteBites application, including Customer, Item, Transaction, and PurchaseHistory. Each class includes relevant attributes and methods to manage the application's core functionality.
+
+Customer - customer entity that tracks name and purchase history
+Item - item entity that tracks name and price
+Transaction - transaction entity that tracks customer, item, and date of purchase
+PurchaseHistory - purchase history entity that tracks a customer's transactions
+'''
